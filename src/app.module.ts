e83abb69aeb2dbffa4e2a485e3bec576e8774461
Luan Bianchini <luanbianchine1@gmail.com>
2023-10-03 16:50:42 -0300

    style: add eslint style

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './presentation/controllers/app.controller';
-import { AppService } from './data/services/app.service';
+import { Module } from "@nestjs/common";
+import { AppService } from "./data/services/app.service";
+import { AppController } from "./presentation/controllers/app.controller";
 
 @Module({
   imports: [],
--- src/presentation/controllers/app.controller.ts
+++ src/presentation/controllers/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from '../../data/services/app.service';
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "../../data/services/app.service";
 
 @Controller()
 export class AppController {
