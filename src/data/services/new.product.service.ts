9d930dc8a6de7657c84da7f94046639a9fa9cfa2
Luan Bianchini <luanbianchine1@gmail.com>
2023-10-19 16:19:13 -0300

    feat: return entity product

--- src/data/services/new.product.service.ts
+++ src/data/services/new.product.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from "@nestjs/common";
+import { randomUUID } from "crypto";
+import { ProductEntity, ProductEntityProps } from "src/domain/entities/product.entity";
 import { DbProduct } from "src/infra/repository/db.product.table";
 import { NewProductDto } from "src/main/core/dtos/product.dto";
 
@@ -6,8 +8,17 @@ import { NewProductDto } from "src/main/core/dtos/product.dto";
 export class ProductService {
   constructor(private readonly repository: DbProduct) {}
 
-  async perform(productDto: NewProductDto): Promise<string> {
-    console.log(productDto);
-    return "teste";
+  async perform(productDto: NewProductDto): Promise<ProductEntity> {
+    const product = await this.createProduct(productDto.name, productDto.type);
+    return product;
+  }
+
+  private async createProduct(name: string, type: string) {
+    const productEntityProps: ProductEntityProps = {
+      id: randomUUID(),
+      name,
+      type,
+    };
+    return ProductEntity.create(productEntityProps);
   }
 }
